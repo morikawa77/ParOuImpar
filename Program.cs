df6419414107f8bf4f82d8116a97c8b6de7b71f5
REGINALDO MORIKAWA <reginaldo.morikawa@senacsp.edu.br>
2017-08-21 14:56:02 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace ParOuImpar
     {
         static void Main(string[] args)
         {
+            decimal numero, modulo;
+            Console.Write("Digite um número: ");
+            numero = Convert.ToDecimal(Console.ReadLine());
+            modulo = numero % 2;
+            if (modulo == 0)
+            {
+                Console.Write("O número digitado é PAR");
+            }else
+            {
+                Console.Write("O número digitado é ÍMPAR");
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+"Calcula se o n£mero ‚ par ou ¡mpar C# Curso Programa‡Æo de Dispositivos M¢veis SENAC" 
